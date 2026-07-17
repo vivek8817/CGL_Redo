186220c381e5253277329686935c61527d303a9e
vivek8817 <vivekcbba@gmail.com>
2026-07-17 14:42:11 +0530

    Fix CORS logic to allow Vercel domains dynamically

--- Backend_Redo/src/app.ts
+++ Backend_Redo/src/app.ts
@@ -22,7 +22,9 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || 
+        allowedOrigins.indexOf(origin) !== -1 || 
+        origin.endsWith('.vercel.app')) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
